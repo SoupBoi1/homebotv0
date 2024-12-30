8e77da9f2ff43c0de8b8558e706c197b59a6bd41
Sudipta Halder <66485000+SoupBoi1@users.noreply.github.com>
2024-12-30 10:33:06 -0600

    mpu6050

--- MPU6050.py
+++ MPU6050.py
@@ -1,78 +1,33 @@
-import smbus					#import SMBus module of I2C
-from time import sleep          #import
+# Import the mpu6050 class and sleep function from respective modules.
+from mpu6050 import mpu6050
+from time import sleep
 
-#some MPU6050 Registers and their Address
-PWR_MGMT_1   = 0x6B
-SMPLRT_DIV   = 0x19
-CONFIG       = 0x1A
-GYRO_CONFIG  = 0x1B
-INT_ENABLE   = 0x38
-ACCEL_XOUT_H = 0x3B
-ACCEL_YOUT_H = 0x3D
-ACCEL_ZOUT_H = 0x3F
-GYRO_XOUT_H  = 0x43
-GYRO_YOUT_H  = 0x45
-GYRO_ZOUT_H  = 0x47
+# Initialize the MPU-6050 sensor with the I2C address 0x68.
+sensor = mpu6050(0x68)
 
-
-def MPU_Init():
-	#write to sample rate register
-	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
-	
-	#Write to power management register
-	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
-	
-	#Write to Configuration register
-	bus.write_byte_data(Device_Address, CONFIG, 0)
-	
-	#Write to Gyro configuration register
-	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
-	
-	#Write to interrupt enable register
-	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
-
-def read_raw_data(addr):
-	#Accelero and Gyro value are 16-bit
-        high = bus.read_byte_data(Device_Address, addr)
-        low = bus.read_byte_data(Device_Address, addr+1)
-    
-        #concatenate higher and lower value
-        value = ((high << 8) | low)
-        
-        #to get signed value from mpu6050
-        if(value > 32768):
-                value = value - 65536
-        return value
-
-
-bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
-Device_Address = 0x68   # MPU6050 device address
-
-MPU_Init()
-
-print (" Reading Data of Gyroscope and Accelerometer")
-
+# Infinite loop to continuously read data from the sensor.
 while True:
-	
-	#Read Accelerometer raw value
-	acc_x = read_raw_data(ACCEL_XOUT_H)
-	acc_y = read_raw_data(ACCEL_YOUT_H)
-	acc_z = read_raw_data(ACCEL_ZOUT_H)
-	
-	#Read Gyroscope raw value
-	gyro_x = read_raw_data(GYRO_XOUT_H)
-	gyro_y = read_raw_data(GYRO_YOUT_H)
-	gyro_z = read_raw_data(GYRO_ZOUT_H)
-	
-	#Full scale range +/- 250 degree/C as per sensitivity scale factor
-	Ax = acc_x/16384.0
-	Ay = acc_y/16384.0
-	Az = acc_z/16384.0
-	
-	Gx = gyro_x/131.0
-	Gy = gyro_y/131.0
-	Gz = gyro_z/131.0
-	
+    # Retrieve accelerometer data from the sensor.
+    accel_data = sensor.get_accel_data()
+    # Retrieve gyroscope data from the sensor.
+    gyro_data = sensor.get_gyro_data()
+    # Retrieve temperature data from the sensor.
+    temp = sensor.get_temp()
 
-	print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
-	sleep(1)
+    # Print accelerometer data.
+    print("Accelerometer data")
+    print("x: " + str(accel_data['x']))
+    print("y: " + str(accel_data['y']))
+    print("z: " + str(accel_data['z']))
+
+    # Print gyroscope data.
+    print("Gyroscope data")
+    print("x: " + str(gyro_data['x']))
+    print("y: " + str(gyro_data['y']))
+    print("z: " + str(gyro_data['z']))
+
+    # Print the temperature in Celsius.
+    print("Temp: " + str(temp) + " C")
+
+    # Pause for 0.5 seconds before the next read cycle.
+    sleep(0.5)
