211ea4b6c6b31d9d865f143331c9d046011e84cb
Sudipta Halder <66485000+SoupBoi1@users.noreply.github.com>
2024-12-30 10:59:57 -0600

    jk

--- Gyro.py
+++ Gyro.py
@@ -1,19 +1,19 @@
-import mpu6050
+from mpu6050 import mpu6050 
 import time
 
 # Create a new Mpu6050 object
-mpu6050 = mpu6050(0x68)
+gyro = mpu6050(0x68)
 
 # Define a function to read the sensor data
 def read_sensor_data():
     # Read the accelerometer values
-    accelerometer_data = mpu6050.get_accel_data()
+    accelerometer_data = gyro.get_accel_data()
 
     # Read the gyroscope values
-    gyroscope_data = mpu6050.get_gyro_data()
+    gyroscope_data = gyro.get_gyro_data()
 
     # Read temp
-    temperature = mpu6050.get_temp()
+    temperature = gyro.get_temp()
 
     return accelerometer_data, gyroscope_data, temperature
 
