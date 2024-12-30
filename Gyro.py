a8bcf54e4aa2843703490d70af72d02f612e366a
Sudipta Halder <66485000+SoupBoi1@users.noreply.github.com>
2024-12-30 11:01:11 -0600

    kj

--- Gyro.py
+++ Gyro.py
@@ -2,7 +2,7 @@ from mpu6050 import mpu6050
 import time
 
 # Create a new Mpu6050 object
-gyro = mpu6050(0x68)
+gyro = mpu6050(address=0x68)
 
 # Define a function to read the sensor data
 def read_sensor_data():
