b5426d4e9b881f2ba654626a79dd424f508d96e2
Sudipta Halder <66485000+SoupBoi1@users.noreply.github.com>
2025-01-12 17:19:47 -0600

    control userr input

--- xboxcon.py
+++ xboxcon.py
@@ -5,17 +5,12 @@ pygame.init()
 pygame.joystick.init()
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
-turn =[0.0,0.0]
 
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.JOYAXISMOTION:
-            if event.axis == 0:
-                turn[0] =event.value
-            if event.axis ==1:
-                turn[1] =event.value
-            #print(f"Axis {event.axis} value: {event.value}")
+            print(f"Axis {event.axis} value: {event.value}")
         elif event.type == pygame.JOYBUTTONDOWN:
             print(f"Button {event.button} pressed")
         elif event.type == pygame.JOYBUTTONUP:
