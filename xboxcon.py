3637cf06335cf2f8080011a52a3c1ea4c25b1a41
SoupBoi1 <real.sudipta.halder@gamil.com>
2025-01-12 13:57:26 -0600

    added

--- xboxcon.py
+++ xboxcon.py
@@ -1,16 +1,21 @@
 import pygame
+import numpy as np 
 
 pygame.init()
 pygame.joystick.init()
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
-
+turn =[0.0,0.0]
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.JOYAXISMOTION:
-            print(f"Axis {event.axis} value: {event.value}")
+    	if event.type == pygame.JOYAXISMOTION:
+            if event.axis == 1:
+                turn[0] =event.value
+            if event.axis ==0:
+                turn[1] =event.value
+            #print(f"Axis {event.axis} value: {event.value}")
         elif event.type == pygame.JOYBUTTONDOWN:
             print(f"Button {event.button} pressed")
         elif event.type == pygame.JOYBUTTONUP:
             print(f"Button {event.button} released")
-
+	print(turn)
