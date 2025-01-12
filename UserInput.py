668d76f9fb5e8e60a34d260049727f8df857f6b8
SoupBoi1 <real.sudipta.halder@gamil.com>
2025-01-12 16:30:08 -0600

    edit

--- UserInput.py
+++ UserInput.py
@@ -8,7 +8,7 @@ class UserInput:
         pygame.joystick.init()
         joystick = pygame.joystick.Joystick(0)
         joystick.init()
-    def getRawInputs():
+    def getRawInputs(self):
         #ls X, ls Y, rs X, rs Y, LT,RT,
         RawInput =[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
         for event in pygame.event.get():
@@ -20,21 +20,3 @@ class UserInput:
             elif event.type == pygame.JOYBUTTONUP: # down is -1
                 print(f"Button {event.button} released")
         return RawInput
-
-
-
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.JOYAXISMOTION:
-            if event.axis == 0:
-                turn[0] =event.value
-            if event.axis ==1:
-                turn[1] =event.value
-            #print(f"Axis {event.axis} value: {event.value}")
-        elif event.type == pygame.JOYBUTTONDOWN:
-            print(f"Button {event.button} pressed")
-        elif event.type == pygame.JOYBUTTONUP:
-            print(f"Button {event.button} released")
-    print(f"[{round(turn[0],1)},{round(turn[1],1)}]") 
-
