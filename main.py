eb937b8ed8838df4da1c5b0eb4f87ee3de4fef9f
Sudipta Halder <66485000+SoupBoi1@users.noreply.github.com>
2025-01-12 22:18:26 -0600

    controller trasnlator

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mpu6050 import mpu6050
 import time 
 import pygame
 from L298NMoterDriver import L298NMoterDriver as motor
+from ControlerTranslator import ControlerTranslator as ct
 
 mpu = mpu6050(0x68)
 
@@ -15,6 +16,7 @@ motor.init()
 
 contols_turn =[0.0,0.0]
 contols_acc =[0.0,0.0]
+contols_acc_1D =0.0
 contols_t = False
 
 accel_data = mpu.get_accel_data()
@@ -28,21 +30,26 @@ while(True):
 				contols_turn[event.axis] =event.value
 			if event.axis == 4:
 				contols_acc[0] = (round(event.value,1)+1.0)/2.0
+				contols_acc_1D = contols_acc[0]
+				motor.config_forward()
 			if event.axis == 5:
 				contols_acc[1] = (round(event.value,1)+1.0)/2.0
+				contols_acc_1D = -contols_acc[1]
+				motor.config_backward()
 		elif event.type == pygame.JOYBUTTONUP:
-                        if event.button ==4:
-                               contols_t = not contols_t
-                        print(f"Button {event.button} released")
+			if event.button ==4:
+					contols_t = not contols_t
+			print(f"Button {event.button} released")
 	print(f"[{round(contols_turn[0],1)},{round(contols_turn[1],1)}] speed: {contols_acc}") 
-	
-	motor._pwm_d=[contols_acc[0]*100.0,contols_acc[1]*100.0]
-	#motor.edit_PWM_D([contols_acc[1]*100.0,contols_acc[0]*100.0])
+	motor._pwm_d = ControlerTranslator.get(contols_turn,contols_acc_1D)
+	#motor._pwm_d=[contols_acc[0]*100.0,contols_acc[1]*100.0]
+	#motor.edit_PWM_D([contols_acc[1]*100.0,contols_acc[0]*100.0])motor._pwm_d()
+
 	motor.update_PWM_D()
-	if contols_t:
-            motor.config_backward()
-	else:
-	    motor.config_forward()
+	#if contols_t:
+    #        motor.config_backward()
+	#else:
+	#    motor.config_forward()
 	accel_data = mpu.get_accel_data()
 	gyro_data = mpu.get_gyro_data()
 	print(mpu.get_temp())
