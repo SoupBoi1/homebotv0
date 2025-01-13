22114feb50e0a172bbab61f45691d9615dd7c1ad
Sudipta Halder <66485000+SoupBoi1@users.noreply.github.com>
2025-01-12 23:43:11 -0600

    sonar

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import pygame
 from L298NMoterDriver import L298NMoterDriver as motor
 import ControlerTranslator
+import sonar 
 
 mpu = mpu6050(0x68)
 
@@ -12,48 +13,48 @@ joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
 motor.init()
+sonar.init()
 
 
 contols_turn =[0.0,0.0]
 contols_acc =[0.0,0.0]
-contols_acc_1D =0.0
 contols_t = False
 
 accel_data = mpu.get_accel_data()
 
 gyro_data = mpu.get_gyro_data()
-
-while(True):
-	for event in pygame.event.get():
-		if event.type == pygame.JOYAXISMOTION:
-			if event.axis <= 1:
-				contols_turn[event.axis] =event.value
-			if event.axis == 4:
-				contols_acc[0] = (round(event.value,1)+1.0)/2.0
-				contols_acc_1D = contols_acc[0]
-				motor.config_forward()
-			if event.axis == 5:
-				contols_acc[1] = (round(event.value,1)+1.0)/2.0
-				contols_acc_1D = -contols_acc[1]
-				motor.config_backward()
-		elif event.type == pygame.JOYBUTTONUP:
-			if event.button ==4:
-					contols_t = not contols_t
-			print(f"Button {event.button} released")
-	print(f"[{round(contols_turn[0],1)},{round(contols_turn[1],1)}] speed: {contols_acc_1D}") 
-	motor._pwm_d = ControlerTranslator.get(contols_turn,contols_acc[0]+contols_acc[1],100)
-	#motor._pwm_d=[contols_acc[0]*100.0,contols_acc[1]*100.0]
-	#motor.edit_PWM_D([contols_acc[1]*100.0,contols_acc[0]*100.0])motor._pwm_d()
-	print(motor._pwm_d)
-	motor.update_PWM_D()
-	#if contols_t:
-    #        motor.config_backward()
-	#else:
-	#    motor.config_forward()
-	accel_data = mpu.get_accel_data()
-	gyro_data = mpu.get_gyro_data()
-	print(mpu.get_temp())
-	print(f" acc: {accel_data['x']}, {accel_data['y']}, {accel_data['z']}")
-	print(f" gyro: {gyro_data['x']}, {gyro_data['y']}, {gyro_data['z']}")
-	time.sleep(.1)
-	
+try: 
+		
+	while(True):
+		for event in pygame.event.get():
+			if event.type == pygame.JOYAXISMOTION:
+				if event.axis <= 1:
+					contols_turn[event.axis] =event.value
+				if event.axis == 4:
+					contols_acc[0] = (round(event.value,1)+1.0)/2.0
+					motor.config_forward()
+				if event.axis == 5:
+					contols_acc[1] = (round(event.value,1)+1.0)/2.0
+					motor.config_backward()
+			elif event.type == pygame.JOYBUTTONUP:
+				if event.button ==4:
+						contols_t = not contols_t
+				print(f"Button {event.button} released")
+		print(f"[{round(contols_turn[0],1)},{round(contols_turn[1],1)}] speed: {contols_acc_1D}") 
+		motor._pwm_d = ControlerTranslator.get(contols_turn,contols_acc[0]+contols_acc[1],100)
+		#motor.edit_PWM_D([contols_acc[1]*100.0,contols_acc[0]*100.0])motor._pwm_d()
+		print(motor._pwm_d)
+		motor.update_PWM_D()
+		#if contols_t:
+		#        motor.config_backward()
+		#else:
+		#    motor.config_forward()
+		sonar.getDistance()
+		accel_data = mpu.get_accel_data()
+		gyro_data = mpu.get_gyro_data()
+		print(mpu.get_temp())
+		print(f" acc: {accel_data['x']}, {accel_data['y']}, {accel_data['z']}")
+		print(f" gyro: {gyro_data['x']}, {gyro_data['y']}, {gyro_data['z']}")
+		time.sleep(.1)
+except KeyboardInterrupt:
+        GPIO.cleanup()
--- sonar.py
+++ sonar.py
@@ -2,35 +2,27 @@ import RPi.GPIO as GPIO
 import time
 
 
-GPIO.setmode(GPIO.BCM)
 
 PIN_TRIGGER = 5
 PIN_ECHO = 6
+def init():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+    GPIO.setup(PIN_ECHO, GPIO.IN)
 
-GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-GPIO.setup(PIN_ECHO, GPIO.IN)
-
-GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-print("Waiting for sensor to settle")
-
-time.sleep(2)
-
-print("Calculating distance")
-
-GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-
-time.sleep(0.00001)
-
-GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-while GPIO.input(PIN_ECHO)==0:
-    pulse_start_time = time.time()
-while GPIO.input(PIN_ECHO)==1:
-    pulse_end_time = time.time()
-
-pulse_duration = pulse_end_time - pulse_start_time
-distance = round(pulse_duration * 17150, 2)
-print ("Distance:",distance,"cm")
-
-GPIO.cleanup()
+def getDistance():
+    GPIO.output(PIN_TRIGGER, GPIO.LOW)
+    print("Calculating distance")
+    GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+    time.sleep(0.00001)
+    GPIO.output(PIN_TRIGGER, GPIO.LOW)
+    while GPIO.input(PIN_ECHO)==0:
+        pulse_start_time = time.time()
+    while GPIO.input(PIN_ECHO)==1:
+        pulse_end_time = time.time()
+    pulse_duration = pulse_end_time - pulse_start_time
+    distance = round(pulse_duration * 17150, 2)
+    #print ("Distance:",distance,"cm")
+    return  distance
+def end():
+    GPIO.cleanup()
