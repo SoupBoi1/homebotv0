cc982cf7bbdb1429722b561021eeaeb498f85bea
Sudipta Halder <66485000+SoupBoi1@users.noreply.github.com>
2025-01-12 22:27:36 -0600

    printign hot fix

--- ControlerTranslator.py
+++ ControlerTranslator.py
@@ -6,17 +6,17 @@ def clamp(n, min_value, max_value):
 #contols_acc =[0.0,0.0]
 
 #contols_acc_1D =0.0
-def get(contols_turn,contols_acc_1D):
+def get(contols_turn,contols_acc_1D,speed=100):
     TmotorA = 0
     TmotorB =0
     power = (contols_acc_1D+1)/2
 
     if contols_turn[0]*contols_acc_1D <0:
-        TmotorA = -contols_acc_1D+1
+        TmotorA = -contols_turn[0]+1
         TmotorA = clamp(TmotorB,0,1)
         TmotorB= 1.0
     elif contols_turn[0]*contols_acc_1D >0:
-        TmotorB = contols_acc_1D+1
+        TmotorB = contols_turn[0]+1
         TmotorB = clamp(TmotorB,0,1)
         TmotorA =1.0
     else:
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from mpu6050 import mpu6050
 import time 
 import pygame
 from L298NMoterDriver import L298NMoterDriver as motor
-from ControlerTranslator import ControlerTranslator as ct
+import ControlerTranslator
 
 mpu = mpu6050(0x68)
 
@@ -41,7 +41,7 @@ while(True):
 					contols_t = not contols_t
 			print(f"Button {event.button} released")
 	print(f"[{round(contols_turn[0],1)},{round(contols_turn[1],1)}] speed: {contols_acc}") 
-	motor._pwm_d = ControlerTranslator.get(contols_turn,contols_acc_1D)
+	motor._pwm_d = ControlerTranslator.get(contols_turn,contols_acc_1D,100)
 	#motor._pwm_d=[contols_acc[0]*100.0,contols_acc[1]*100.0]
 	#motor.edit_PWM_D([contols_acc[1]*100.0,contols_acc[0]*100.0])motor._pwm_d()
 	print(motor._pwm_d)
